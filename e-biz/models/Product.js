3b680a2ff2ef9b07fedde3bd31cf2cf222163336
William Bai <williambai2001@gmail.com>
2015-05-26 17:52:29 +0800

    update e-biz project.

--- e-biz/models/Product.js
+++ e-biz/models/Product.js
@@ -35,29 +35,6 @@ exports = module.exports = function(app, config, mongoose, nodemailer){
 	
 	mongoose.productSchema = productSchema;
 
-	var userSchema = new mongoose.Schema({
-		username: {type: String},
-		email: {type: String},
-		first_name: {type: String},
-		last_name: {type: String},
-		hashed_password: {type: String},
-		address: [
-			{
-				name: {type: String},
-				street: {type: String},
-				city: {type: String},
-				state: {type: String},
-				zip: {type: Number}
-			}
-		],
-		payment_methods: [{
-			name: {type: String},
-			last_four: {type: Number},
-			crypted_number: {type: Number},
-			expiration_date: {type: Date}
-		}]
-	});
-
 	var Product = mongoose.model('Product', productSchema);
 
 	var defaultCallback = function(err){
