1d79841de10b05381547469912cae07c940163f4
William Bai <williambai2001@gmail.com>
2015-06-24 09:25:04 +0800

    update demo project.

--- demo/commands/wx_mp_command.js
+++ demo/commands/wx_mp_command.js
@@ -1,15 +1,21 @@
 var config = require('../config/weixin');
 var mpApi = require('../libs/weixin_api').mpApi;
+var path = require('path');
 var async = require('async');
-
-var menu = require('./weixin/menu');
+var jf = require('jsonfile');
 
 async.waterfall(
 	[
 		//更新菜单
 		function(callback){
+			var menu = jf.readFileSync(path.join(__dirname,'./weixin/menu.json'));
 			mpApi.createMenu(menu,callback);
 		},
+		// 新增素材
+		// function(callback){
+		// 	var about_us = jf.readFileSync(path.join(__dirname,'./weixin/about_us.json'));
+		// 	mpApi.uploadNewsMaterial(about_us,callback);
+		// }
 	]
 	,function _result(err,results){
 		if(err){
