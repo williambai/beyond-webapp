68392970dcf2dc0e82e979dbc452b06579238301
William Bai <williambai2001@gmail.com>
2016-08-29 07:43:17 +0800

    update unicom project.

--- unicom/app/scripts/publicApp/router.js
+++ unicom/app/scripts/publicApp/router.js
@@ -14,20 +14,11 @@ exports = module.exports = function(appEvents){
 	routesMap = _.extend(routesMap, categoryRouter());
 	routesMap = _.extend(routesMap, productRouter());
 	routesMap = _.extend(routesMap, notFoundRouter());
-	// console.log('routes: ' + JSON.stringify(_.keys(routesMap)));
-	var MyRouter = Router.extend({
-		routes: routesMap,
-	});	
-	var router =new MyRouter({appEvents:appEvents});
 
-
-	//** controller 模式
-	// var categoryController = require('./category/controller')(router);
-	// router.routes = {
-	// 	'': categoryController.index,
-	// 	'category/index': categoryController.index,//** 产品分类
-	// 	// 'category/:cid/products': categoryController.categoryProduct,//** 按分类查找产品
-	// };
-
+	var router =new Router({
+			routes:routesMap,
+			appEvents:appEvents
+		});
+	// console.log('routes: ' + JSON.stringify(_.keys(router.routes)));
 	return router;
 }
